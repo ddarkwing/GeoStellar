da4d2de187dea7eca260823a0540f550865eb990
Joseph Chen <chenjoseph@berkeley.edu>
2022-03-12 15:53:18 -0800

    Added loading screen and LoadingContext

--- geostellar/src/Context.js
+++ geostellar/src/Context.js
@@ -2,3 +2,5 @@ import React, { createContext, Component, useState, setState } from "react";
 import PropTypes from "prop-types";
 
 export const AccountContext = createContext({});
+
+export const LoadingContext = createContext({});
