8b8e484c6409a5cae80718ef82bd1df2e14a3755
mikeyliang <liangmichael27822@gmail.com>
2022-03-12 16:30:51 -0800

    mikey merge joseph

--- geostellar/src/App.js
+++ geostellar/src/App.js
@@ -2,26 +2,27 @@ import Navbar from './components/navbar/Navbar';
 import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import SimpleMap from './Map'
 
-import {AccountContext} from './Context.js';
+import {AccountContext, LoadingContext} from './Context.js';
 
 function App() {
 
   const [publicK, setPublicK] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
 
   return (
 
       <Router>
         <AccountContext.Provider value={{publicK, setPublicK}}>
-          <div className="App">
-            <Navbar />
+          <LoadingContext.Provider value={{loading, setLoading}}>
+            <div className="App">
+              <Navbar />
 
-            <div className="content">
-              <SimpleMap />
+              <div className="content">
+              </div>
+
             </div>
-
-          </div>
+          </LoadingContext.Provider>
         </AccountContext.Provider>
       </Router>
   );
