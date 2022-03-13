87757fc50f8410136b67dd9db483cf7720ac5633
Joseph Chen <chenjoseph@berkeley.edu>
2022-03-13 09:47:30 -0700

    Attempting to implement NFT capabilities like health, attacking, etc

--- geostellar/src/static/Marker.tsx
+++ geostellar/src/static/Marker.tsx
@@ -6,10 +6,19 @@ import { imageListClasses } from '@mui/material';
 import Button from '@mui/material/Button';
 import theme from '../theme'
 import { css } from "@emotion/react"
+import StellarSdk from 'stellar-sdk';
 
 const Marker = (props: any) => {
-    const { color, name, id, lat, lng, desc, img} = props;
+    const { color, name, id, lat, lng, publicKey, desc, img, AssetNum, IssueAccount} = props;
 
+    var markerClass = new String(getStringValue(color))
+
+    function getStringValue(value: any): string {
+      return value.toString();
+  }
+
+    console.log(markerClass)
+    
     function degreesToRadians(degrees) {
       return degrees * Math.PI / 180;
     }
@@ -33,7 +42,158 @@ const Marker = (props: any) => {
 
     const logo = require('../static/img/' + img)
 
+    const setup = {fee: StellarSdk.BASE_FEE, networkPassphrase: StellarSdk.Networks.TESTNET}
+
+    const proximity = async () => {
+
+            try {
+              const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
+      
+              // Add here if statement to check the marker we're point at and see the distance.
+              // so we know which asset to add trustline to
+      
+              const monster = new StellarSdk.Asset({AssetNum}, publicKey)
+              const monster_issuingKey = StellarSdk.Keypair.fromSecret({IssueAccount})
+      
+              if (dis < 2) {
+                const acc = await server.loadAccount(publicKey)
+                const paymentXLM = 2/dis;
+      
+                const accPay = new StellarSdk.TransactionBuilder(acc, setup)
+                .addOperation(
+                  StellarSdk.Operation.setOptions({
+                    //Add homedomain
+                    signer: {ed25519PublicKey: publicKey,
+                            weight: 0},
+                    source: monster_issuingKey
+                  })
+                )
+                .addOperation(
+                  StellarSdk.Operation.changeTrust({
+                    asset: monster,
+                    limit: '5',
+                  })
+                )
+                .addOperation(
+                  StellarSdk.Operation.payment({
+                    destination: publicKey,
+                    asset: monster,
+                    amount: paymentXLM,
+                  })
+                )
+                .addOperation(
+                  StellarSdk.Operation.payment({
+                    destination: monster_issuingKey.publicKey(),
+                    asset: StellarSdk.Asset.native(),
+                    amount: 5,
+                  })
+                ).setTimeout(180).build()
+      
+                // accPay.sign(monster_issuingKey, secretKey)
+      
+                server.submitTransaction(accPay)
+      
+              } else {
+                console.log("You are too far away to mint")
+              }
+            
+      
+            } catch (error) {
+              console.log("Marker not hovered/Funds not enough!")
+            }
+              
+          }
+
+      const getHP = async () => {
+              try {
+        
+                const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
+        
+                const monster_issuingKey = StellarSdk.Keypair.fromSecret('SCZQKI2IEW3HU762LFK7RNVA6IIVS23DFDH4JKW6Q737KLWRVT4KVFHY');
+                const acc = await server.loadAccount(monster_issuingKey.publicKey());
+        
+                Object.entries(acc.data_attr).forEach(entry => {
+                  const [key, value] = entry;
+                  if (typeof value === 'string') {
+                    if (key === 'HP') {
+                      var b = Buffer.from(value, 'base64')
+                      console.log(b.toString())
+                      return parseInt(b.toString());
+                    
+                    }
+                  }
+                });
+                return 0;
+        
+              } catch (error) {
+                console.log("No HP found!");
+                return 0;
+              }
+            }
+    
+        const getTop = async() => {
+                  try {
+                    var maxAcc = ''
+                    var maxDmg = 0
+                    const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
+          
+                    const monster_issuingKey = StellarSdk.Keypair.fromSecret({IssueAccount});
+                    const acc = await server.loadAccount(monster_issuingKey.publicKey());
+                    acc.signers.forEach((element) => {
+          
+                      if (element !== monster_issuingKey.publicKey()) {
+          
+                        const signers = async (element) => {
+          
+                          const signerAcc = await server.loadAccount(element['key'])
+                          signerAcc.balances.forEach((asset) => {
+                            if (asset.asset_type === {AssetNum}) {
+                              if (asset.balance > maxDmg) {
+                                maxAcc = element;
+                                maxDmg = asset.balance
+                              }
+                            }
+                          });
+                        }
+                      }
+                    });
+                    return [maxAcc, maxDmg];
+          
+                  } catch (error) {
+                    console.log("Could not find Max Dmg User!")
+                    return ['', 0];
+                  }
+              }
+    
+    const claimAndAttack = async() => {
+      if (markerClass === "green") {
+        proximity();
+      } else if (markerClass === "red") {
+        const hp = await getHP()
+        if (hp <= 0) {
+          proximity();
+        }
+      }
+    }
+
+    const display = async() => {
+      // create a new div element
+      const currentDivHealth = document.getElementById("display-health");
+      const currentDivTop = document.getElementById("display-top");
+    
+      const health = await getHP()
+      // and give it some content
+      console.log(currentDivHealth)
+
+      if (currentDivHealth !== null && currentDivTop !== null && markerClass !== null) {
+        currentDivHealth.textContent = "Health: " + health + AssetNum
+        currentDivTop.textContent = "Top Hitter: " + health + IssueAccount
+      }
+
+    }
+
     console.log(logo)
+    display();
     
     return (
       <div>
@@ -42,7 +202,8 @@ const Marker = (props: any) => {
     trigger={<div className="marker"
     style={{ backgroundColor: color, cursor: 'pointer', fontSize: '25px'}}
     title={"Distance: " + dis.toString().substring(0, dis.toString().indexOf(".")+3) + " miles"} 
-    onMouseEnter={() => console.log(dis)}
+    onMouseEnter={() => 
+      Marker}
   >      </div>}
     modal
     nested
@@ -54,8 +215,12 @@ const Marker = (props: any) => {
         </button>
         <div className="header"> {name} </div>
         <SplitPane split="vertical" allowResize={false}>
-          <div className="content" style ={{padding: '50px 10px', display: 'flex', justifyContent: 'center'}}> 
-            <img src={logo} style = {{width: '20vw', height:'20vw'}}/><img></img>
+          <div className="content" style ={{padding: '50px 10px', display: 'block', position: "relative"}}> 
+            <img src={logo} style = {{width: '20vw', height:'20vw'}}/>
+
+            <div id="display-health" style = {{position: "relative"}}>Health</div>
+
+            <div id="display-top" style = {{position: "relative"}}>Top Hitters </div>
           </div>
           <div className="content" style={{padding: '50px 20px', fontSize: '1vw', display: 'flex', justifyContent: 'center'}}>{desc}</div>
 
@@ -66,7 +231,8 @@ const Marker = (props: any) => {
           <Button variant="outlined"
               className="button" style={{borderColor: '#3E1BDB', marginRight: '20px'}}
               onClick={() => {
-                console.log('function 1 button');
+                console.log('function 1 button pressed');
+                proximity();
               }}>
               5 XLM
           </Button>
@@ -75,7 +241,7 @@ const Marker = (props: any) => {
           <Button variant="outlined"
               className="button" style={{borderColor: '#3E1BDB', marginLeft: '40px'}}
               onClick={() => {
-                console.log('function 2 button');
+                console.log('function 2 button pressed');
               }}>
               function 2
           </Button>
